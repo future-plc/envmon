69030c8d9e142347ae59c8c89bcdebfef074677a
snee <andy@gumgum.studio>
2024-02-09 10:32:00 -0500

    aqi sensor working

--- src/envmon/main.py
+++ src/envmon/main.py
@@ -1,38 +1,34 @@
-from sensors import AQISensor
+from sensors import Sensor, AQISensor
 from plotting import Plotter, Environment
 from timer import Timer
+import time
 import board
 from busio import I2C
 import logging
 import argparse
 
-i2c = I2C(board.SCL, board.SDA, frequency=100000)
-my_sensors = [AQISensor(i2c)]
 UPDATE_INTERVAL = 1000
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--debug", help="Enable debug logging")
+parser.add_argument("-v", "--debug", help="Enable debug logging", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.WARNING)
 
-
-
-
-
-def update() -> Environment:
+def update(s: list[Sensor]) -> Environment:
     for s in my_sensors:
         results = s.read()
     return
 
 
 timer = Timer()
-plot = Plotter()
+# plot = Plotter()
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel)
+    i2c = I2C(board.SCL, board.SDA, frequency=100000)
+    aqi = AQISensor(i2c)
+    my_sensors = [aqi]
+    
+    time.sleep(2)
 
-    if args.debug:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.WARN)
-
-    update()
+    update(my_sensors)
     # plot.draw()
     # timer.tick()
